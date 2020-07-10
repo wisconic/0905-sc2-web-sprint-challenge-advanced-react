1f1a682e03ba86fa9aaae609ca2d7abf9f118b1d
Nic Lehman <niclehman@Nics-MacBook-Pro.local>
2020-07-10 11:25:22 -0500

    🧪 TEST: Checkout form header does render

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,16 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import CheckoutForm from './CheckoutForm';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test('form header renders', () => {
+  // [1] arrange
+  render(<CheckoutForm />);
+  // [2] act
+  const header = screen.getByText(/checkout form/i);
+  // [3] assert
+  expect(header).toBeInTheDocument();
+});
 
-test("form shows success message on submit with form details", () => {});
+test('form shows success message on submit with form details', () => {});
